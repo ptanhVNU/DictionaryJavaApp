f5c9f310a78ad4139667354b57a50e0e384199b5
ptanhVNU <ptanh290402@gmail.com>
2022-07-13 21:04:23 +0700

    commandline ver 2

--- src/main/java/DictionaryCommandLine.java
+++ src/main/java/DictionaryCommandLine.java
@@ -6,7 +6,7 @@ public class DictionaryCommandLine {
 
     public void showAllWords() {
         ArrayList<Word> listWords = Dictionary.listWord;
-        System.out.printf("%-7s| %-20s| %-50s\n","No", "English", "Vietnamese");
+        System.out.printf("%-7s| %-20s| %-50s\n", "No", "English", "Vietnamese");
         for (int i = 0; i < listWords.size(); i++) {
             System.out.printf("%-7d| %-20s| %-50s\n", i, listWords.get(i).getWord_target(),
                     listWords.get(i).getWord_explain());
@@ -18,9 +18,14 @@ public class DictionaryCommandLine {
         this.showAllWords();
     }
 
+    public void dictionaryAdvanced() {
+        dictionaryManagement.insertFromFile();
+        this.showAllWords();
+        dictionaryManagement.dictionaryLookup();
+    }
+
     public static void main(String[] args) {
         DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-        dictionaryCommandLine.dictionaryBasic();
-
+        dictionaryCommandLine.dictionaryAdvanced();
     }
 }
--- src/main/java/DictionaryManagement.java
+++ src/main/java/DictionaryManagement.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class DictionaryManagement {
+    final String dictionaryFilePath = "E:\\ProjectJava\\Dictionary\\src\\main\\resources\\data\\dictionaries.txt";
 
-    public void insertFromCommandline(){
+    public void insertFromCommandline() {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Nhap so luong tu ban muon them: ");
-        int numsWord = 0;
+        System.out.print("Nhap so luong tu ban muon them: ");
+        int numsWord;
         numsWord = Integer.parseInt(scanner.nextLine());
 
         for (int i = 0; i < numsWord; i++) {
@@ -23,7 +25,26 @@ public class DictionaryManagement {
             word.setWord_target(target);
             word.setWord_explain(explain);
             Dictionary.listWord.add(word);
-
         }
+    }
+
+    public void insertFromFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(dictionaryFilePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\t");
+                Word word = new Word();
+                word.setWord_target(words[0]);
+                word.setWord_explain(words[words.length - 1]);
+                Dictionary.listWord.add(word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dictionaryLookup() {
+
     }
 }
