90281a2c5b25c9816e7b9c38212f394d43ef0e58
Nguyen Minh Chien <68960736+a2incognito@users.noreply.github.com>
2022-07-30 14:17:02 +0700

    Revert "xin loi cau chen"
    This reverts commit 4f2b52df76d07dedc2309fd8870ec7c89613e943.

--- src/main/java/data/DictionaryManagement.java
+++ src/main/java/data/DictionaryManagement.java
@@ -96,7 +96,7 @@ public class DictionaryManagement extends Dictionary {
   }
 
   /** find out on another Dictionary */
-  public void handleExport(ArrayList<String> list, DictionaryManagement another) {
+  void handleExport(ArrayList<String> list, DictionaryManagement another) {
     ArrayList<Word> words;
     for (String key : list) {
       dictionaryAddWord(another.searchWord(key));
