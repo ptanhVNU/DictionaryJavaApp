67c595871e383151703e6f14a24cd0ad2a62b589
Tuan Anh <83297799+ptanhVNU@users.noreply.github.com>
2022-08-05 16:46:48 +0700

    add exportDB (test)

--- src/main/java/data/JDBCConnect.java
+++ src/main/java/data/JDBCConnect.java
@@ -1,68 +1,76 @@
 package data;
 
-import jdk.nashorn.internal.scripts.JD;
-
 import java.sql.*;
 import java.util.ArrayList;
 
-
 public class JDBCConnect {
-    final String url = "jdbc:mysql://localhost:3306/dictionary";
-    String password = "matkhau123";
-    String username = "root";
-    static Dictionary dictionary = new Dictionary();
+    static final String url = "jdbc:mysql://localhost:3306/dictionary";
+    static String password = "matkhau123";
+    static String username = "root";
+    static ArrayList<Word> words = new ArrayList<>();
 
-    public ArrayList<Word> importDatabase() throws SQLException {
+    public static ArrayList<Word> importDatabase() throws SQLException {
         Connection connection = DriverManager.getConnection(url, username, password);
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT * FROM english_vietnamese");
         int index = 0;
+        int test = 140000;
         while (resultSet.next()) {
-
+            --test;
+            if (test == 0) break;
+            String idx = resultSet.getString("idx");
             String word_target = resultSet.getString("word");
             String detail = resultSet.getString("detail");
 
-
-            StringBuilder explanations = new StringBuilder();
-            StringBuilder pronunciation = new StringBuilder();
-            String usages = "";
+            ArrayList<String> usages = new ArrayList<>();
             String[] res;
             res = detail.split(">");
-            pronunciation.append(res[0]);
+            Word word = new Word();
+            word.setWord(word_target);
+            word.setPronounciation(res[0]);
+            Word.Detail temporaryDetail = new Word.Detail();
             for (int i = 1; i < res.length; i++) {
-                if (res[i].startsWith("*") || res[i].startsWith("-")) {
-                    explanations.append(res[i]).append("\n");
-                } else if (res[i].startsWith("=")){
-                    usages += (res[i] + "\n").replace("=", "");
+                if (res[i].startsWith("*")) {
+                    temporaryDetail = new Word.Detail();
+                    temporaryDetail.setWord_type(res[i]);
+                } else if (res[i].startsWith("-")) {
+                    temporaryDetail.setExplanations(res[i]);
+                } else if (res[i].startsWith("=")) {
+                    temporaryDetail.addUsages(res[i]);
                 }
             }
-            Word word = new Word();
-
-            word.setWord(word_target);
-            word.details.get(index).setPronounciation(pronunciation.toString());
-            word.details.get(index).setExplanations(explanations.toString());
-            word.details.get(index).setUsages(usages);
-            index++;
-            dictionary.wordList.add(word);
+            word.addDetail(temporaryDetail);
+            words.add(word);
         }
         resultSet.close();
         statement.close();
         connection.close();
-        return dictionary.wordList;
+        return words;
     }
 
     // hoan thien sau
-    public void exportDatabase() {
-
+    public void exportDatabase(long size) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dictionary_va", username, password);
+        connection.setAutoCommit(false);
+        connection.getTransactionIsolation();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("INSERT INTO english_vietnamese (idx, word, detail) VALUES (?,?,?);");
+        for (int i = Dictionary.wordList.size() - 1; i >= size; i--) {
+            preparedStatement.setInt(1, i-1);
+            preparedStatement.setString(2, Dictionary.wordList.get(i).getWord());
+            preparedStatement.setString(3, Dictionary.wordList.get(i).getDetails().toString());
+            preparedStatement.addBatch();
+        }
+        preparedStatement.executeBatch();
+        connection.commit();
+        preparedStatement.close();
+        connection.close();
     }
 
     public static void main(String[] args) throws SQLException {
         JDBCConnect jdbcConnect = new JDBCConnect();
-        ArrayList<Word> words = jdbcConnect.importDatabase();
-
-        for (int i = 0; i < words.size(); i++) {
-            System.out.println(words.get(i).getWord());
-            System.out.println(words.get(i).getDetails());
-        }
+        Dictionary.wordList = importDatabase();
+        long size = Dictionary.wordList.size();
+        jdbcConnect.exportDatabase(size);
     }
 }
