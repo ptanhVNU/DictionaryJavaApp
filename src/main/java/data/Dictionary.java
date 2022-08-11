5c0279863a6318327f74c3bc513beeff0bac10e4
Nguyen Minh Chien <68960736+a2incognito@users.noreply.github.com>
2022-08-11 14:45:43 +0700

    edit

--- src/main/java/data/Dictionary.java
+++ src/main/java/data/Dictionary.java
@@ -7,6 +7,119 @@ import java.util.List;
 public class Dictionary {
   protected ArrayList<Word> wordList = new ArrayList<>();
 
+  public Trie root = new Trie();
+
+  private ArrayList<Word> resultsList = new ArrayList<>();
+
+  public void setResultsList(ArrayList<Word> resultsList) {
+    this.resultsList = resultsList;
+  }
+
+  public ArrayList<Word> getResultsList() {
+    return resultsList;
+  }
+
+  /** Adding a new word to dictionary. */
+  public void addNode(Word key) {
+    if (key == null) {
+      return;
+    }
+
+    String key_word = key.getWord();
+    Trie pointer = root;
+
+    for (int i = 0; i < key_word.length(); ++i) {
+      char character = key_word.charAt(i);
+      if (pointer.isExistInChildren(character) == null) {
+        pointer.addChild(character);
+      }
+      pointer = pointer.isExistInChildren(character);
+    }
+
+    pointer.setWord(key);
+    pointer.sort();
+    return;
+  }
+
+  public void getAllWord() {
+    getAllWord(root);
+    return;
+  }
+
+  /** Getting all of word in the Node of dictionary */
+  public void getAllWord(Trie node) {
+    if (node == null) {
+      return;
+    }
+
+    if (node.getWord() != null) {
+      resultsList.add(node.getWord());
+    }
+
+    if (node.getChildren() == null) {
+      return;
+    }
+
+    for (Trie child : node.getChildren()) {
+      getAllWord(child);
+    }
+  }
+
+  /** Find out the node of word. */
+  public Trie findNode(String key) {
+    Trie pointer = root;
+
+    for (int i = 0; i < key.length(); ++i) {
+      char character = key.charAt(i);
+      if (pointer.isExistInChildren(character) == null) {
+        return null;
+      }
+      pointer = pointer.isExistInChildren(character);
+    }
+
+    return pointer;
+  }
+
+  /** Deleting a node (if this node.children empty, delete this node */
+  public void deleteNode(Trie node) {
+    if (node == null) {
+      return;
+    }
+
+    node.setWord(null);
+
+    if (node.getChildren().isEmpty()) {
+      node = null;
+    }
+  }
+
+  public void setWordList() {
+    wordList = resultsList;
+  }
+
+  /** Find out all word have prefix key. */
+  public String searchPrefixWord(String key) {
+    if (key == null) {
+      return "Enter a key";
+    }
+
+    Trie pointer = root;
+    setResultsList(new ArrayList<>());
+
+    for (int i = 0; i < key.length(); ++i) {
+      char character = key.charAt(i);
+      if (pointer.isExistInChildren(character) == null) {
+        return "Not found key";
+      }
+
+      pointer = pointer.isExistInChildren(character);
+    }
+
+    getAllWord(pointer);
+
+    return "resultsList";
+  }
+
   public static class Trie {
     private ArrayList<Trie> children = new ArrayList<>();
 
@@ -106,113 +219,5 @@ public class Dictionary {
         heapify(children, i, 0);
       }
     }
-  }
-
-  public Trie root = new Trie();
-
-  private ArrayList<Word> resultsList = new ArrayList<>();
-
-  public void setResultsList(ArrayList<Word> resultsList) {
-    this.resultsList = resultsList;
-  }
-
-  public ArrayList<Word> getResultsList() {
-    return resultsList;
-  }
-
-  /** Adding a new word to dictionary. */
-  public void addNode(Word key) {
-    if (key == null) {
-      return;
-    }
-
-    String key_word = key.getWord();
-    Trie pointer = root;
-
-    for (int i = 0; i < key_word.length(); ++i) {
-      char character = key_word.charAt(i);
-      if (pointer.isExistInChildren(character) == null) {
-        pointer.addChild(character);
-      }
-      pointer = pointer.isExistInChildren(character);
-    }
-
-    pointer.setWord(key);
-    pointer.sort();
-    return;
-  }
-
-  /** Getting all of word in the Node of dictionary */
-  public void getAllWord(Trie node) {
-    if (node == null) {
-      return;
-    }
-
-    if (node.getWord() != null) {
-      resultsList.add(node.getWord());
-    }
-
-    if (node.getChildren() == null) {
-      return;
-    }
-
-    for (Trie child : node.getChildren()) {
-      getAllWord(child);
-    }
-  }
-
-  /** Find out the node of word. */
-  public Trie findNode(String key) {
-    Trie pointer = root;
-
-    for (int i = 0; i < key.length(); ++i) {
-      char character = key.charAt(i);
-      if (pointer.isExistInChildren(character) == null) {
-        return null;
-      }
-      pointer = pointer.isExistInChildren(character);
-    }
-
-    return pointer;
-  }
-
-  /** Deleting a node (if this node.children empty, delete this node */
-  public void deleteNode(Trie node) {
-    if (node == null) {
-      return;
-    }
-
-    node.setWord(null);
-
-    if (node.getChildren().isEmpty()) {
-      node = null;
-    }
-  }
-
-  public void setWordList() {
-    wordList = resultsList;
-  }
-
-  /** Find out all word have prefix key. */
-  public String searchPrefixWord(String key) {
-    if (key == null) {
-      return "Enter a key";
-    }
-
-    Trie pointer = root;
-    setResultsList(new ArrayList<>());
-
-    for (int i = 0; i < key.length(); ++i) {
-      char character = key.charAt(i);
-      if (pointer.isExistInChildren(character) == null) {
-        return "Not found key";
-      }
-
-      pointer = pointer.isExistInChildren(character);
-    }
-
-    getAllWord(pointer);
-
-    return "resultsList";
   }
 }
--- src/main/java/data/JDBCConnect.java
+++ src/main/java/data/JDBCConnect.java
@@ -4,105 +4,113 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class JDBCConnect {
-    static final String url = "jdbc:mysql://localhost:3306/dictionary";
-    static String password = "matkhau123";
-    static String username = "root";
-    static ArrayList<Word> words = new ArrayList<>();
+  static final String url = "jdbc:mysql://localhost:3306/dictionary";
+  static String password = "matkhau123";
+  static String username = "root";
+  static ArrayList<Word> words = new ArrayList<>();
 
-    public static ArrayList<Word> importDatabase() throws SQLException {
-        Connection connection = DriverManager.getConnection(url, username, password);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM english_vietnamese");
-        int index = 0;
-        int test = 140000;
-        while (resultSet.next()) {
-            --test;
-            if (test == 0) break;
-            String idx = resultSet.getString("idx");
-            String word_target = resultSet.getString("word");
-            String detail = resultSet.getString("detail");
+  public static ArrayList<Word> importDatabase() throws SQLException {
+    Connection connection = DriverManager.getConnection(url, username, password);
+    Statement statement = connection.createStatement();
+    ResultSet resultSet = statement.executeQuery("SELECT * FROM english_vietnamese");
+    int index = 0;
+    int test = 140000;
+    while (resultSet.next()) {
+      --test;
+      if (test == 0) break;
+      String idx = resultSet.getString("idx");
+      String word_target = resultSet.getString("word");
+      String detail = resultSet.getString("detail");
 
-            ArrayList<String> usages = new ArrayList<>();
-            String[] res;
-            res = detail.split(">");
-            Word word = new Word();
-            word.setWord(word_target);
-            word.setPronunciation(res[0]);
-            Word.Detail temporaryDetail = new Word.Detail();
-            for (int i = 1; i < res.length; i++) {
-                if (res[i].startsWith("*")) {
-                    temporaryDetail = new Word.Detail();
-                    temporaryDetail.setWord_type(res[i]);
-                } else if (res[i].startsWith("-")) {
-                    temporaryDetail.setExplanations(res[i]);
-                } else if (res[i].startsWith("=")) {
-                    temporaryDetail.addUsages(res[i]);
-                }
-            }
-            word.addDetail(temporaryDetail);
-            words.add(word);
+      ArrayList<String> usages = new ArrayList<>();
+      String[] res;
+      res = detail.split(">");
+      Word word = new Word();
+      word.setWord(word_target);
+      word.setPronunciation(res[0]);
+      Word.Detail temporaryDetail = new Word.Detail();
+      for (int i = 1; i < res.length; i++) {
+        if (res[i].startsWith("*")) {
+          temporaryDetail = new Word.Detail();
+          temporaryDetail.setWord_type(res[i]);
+        } else if (res[i].startsWith("-")) {
+          temporaryDetail.setExplanations(res[i]);
+        } else if (res[i].startsWith("=")) {
+          temporaryDetail.addUsages(res[i]);
         }
-        resultSet.close();
-        statement.close();
-        connection.close();
-        return words;
+        word.addDetail(temporaryDetail);
+      }
+
+      words.add(word);
     }
+    resultSet.close();
+    statement.close();
+    connection.close();
+    return words;
+  }
 
-        // export database được gọi ngay sau khi thao tác thêm 1 từ mới từ giao diện
-    // thêm từ mới chỉ cần word_target và wơrd_explain
-    // thêm vào cuối danh sách
-    public static void insertToDatabase(ArrayList<Word> wordList) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dictionary_va", username, password);
-        connection.setAutoCommit(false);
-        connection.getTransactionIsolation();
+  // export database được gọi ngay sau khi thao tác thêm 1 từ mới từ giao diện
+  // thêm từ mới chỉ cần word_target và wơrd_explain
+  // thêm vào cuối danh sách
+  public static void insertToDatabase(ArrayList<Word> wordList) throws SQLException {
+    Connection connection =
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/dictionary_va", username, password);
+    connection.setAutoCommit(false);
+    connection.getTransactionIsolation();
 
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO english_vietnamese (idx, word, detail) VALUES (?,?,?);");
-        preparedStatement.setInt(1, wordList.size() + 1);
-        preparedStatement.setString(2, wordList.get(wordList.size() - 1).getWord());
-        preparedStatement.setString(3, wordList.get(wordList.size() - 1).details.get(0).getExplanations());
+    PreparedStatement preparedStatement =
+        connection.prepareStatement(
+            "INSERT INTO english_vietnamese (idx, word, detail) VALUES (?,?,?);");
+    preparedStatement.setInt(1, wordList.size() + 1);
+    preparedStatement.setString(2, wordList.get(wordList.size() - 1).getWord());
+    preparedStatement.setString(
+        3, wordList.get(wordList.size() - 1).details.get(0).getExplanations());
+    preparedStatement.addBatch();
+
+    preparedStatement.executeBatch();
+    connection.commit();
+    preparedStatement.close();
+    connection.close();
+  }
+
+  // cần tìm index của từ cần sửa và xóa
+
+  /**
+   * @param wordList danh sach
+   * @param choice edit or delete
+   * @param index index
+   * @throws SQLException loi SQL
+   */
+  public static void editDatabase(ArrayList<Word> wordList, String choice, int index)
+      throws SQLException {
+    Connection connection =
+        DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/dictionary_va", username, password);
+    connection.setAutoCommit(false);
+    connection.getTransactionIsolation();
+    PreparedStatement preparedStatement = null;
+    switch (choice) {
+      case "edit":
+        String query = "UPDATE english_vietnamese SET detail = ? WHERE idx = ?";
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, wordList.get(index - 2).details.get(0).getExplanations());
+        preparedStatement.setInt(2, index);
         preparedStatement.addBatch();
-
         preparedStatement.executeBatch();
-        connection.commit();
-        preparedStatement.close();
-        connection.close();
+        break;
+      case "delete":
+        String sql = "DELETE FROM english_vietnamese WHERE idx = ?";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, index);
+        preparedStatement.addBatch();
+        preparedStatement.executeBatch();
+        break;
+      default:
+        System.out.println("ERROR");
     }
-
-    // cần tìm index của từ cần sửa và xóa
-    
-    /**
-     * @param wordList danh sach
-     * @param choice   edit or delete
-     * @param index    index
-     * @throws SQLException loi SQL
-     */
-    public static void editDatabase(ArrayList<Word> wordList, String choice, int index) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dictionary_va", username, password);
-        connection.setAutoCommit(false);
-        connection.getTransactionIsolation();
-        PreparedStatement preparedStatement = null;
-        switch (choice) {
-            case "edit":
-                String query = "UPDATE english_vietnamese SET detail = ? WHERE idx = ?";
-                preparedStatement = connection.prepareStatement(query);
-                preparedStatement.setString(1,  wordList.get(index - 2).details.get(0).getExplanations());
-                preparedStatement.setInt(2, index);
-                preparedStatement.addBatch();
-                preparedStatement.executeBatch();
-                break;
-            case "delete":
-                String sql = "DELETE FROM english_vietnamese WHERE idx = ?";
-                preparedStatement = connection.prepareStatement(sql);
-                preparedStatement.setInt(1, index);
-                preparedStatement.addBatch();
-                preparedStatement.executeBatch();
-                break;
-            default:
-                System.out.println("ERROR");
-
-        }
-        connection.commit();
-        preparedStatement.close();
-        connection.close();
-    }
+    connection.commit();
+    preparedStatement.close();
+    connection.close();
+  }
 }
